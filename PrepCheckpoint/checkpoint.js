8e4769bf3adfab4e26322ee4b1b737d18d81dfca
PauloSantiagoFlores <PauloSantiagoFlores@users.noreply.github.com>
2020-03-15 18:34:03 -0300

    Revision Domingo

--- PrepCheckpoint/checkpoint.js
+++ PrepCheckpoint/checkpoint.js
@@ -25,13 +25,21 @@ for (var i =0; i < array.length; i++)
   
   };
 
+
 function menorMayor(numeros) {
 // Escribi una función llamada menorMayor que tome como entrada un arreglo de números y devuelva un arreglo 
 // que contenga el menor número en la posición cero y el mayor número en la posición 1.
 // el arreglo numeros tendra por lo menos dos numeros siempre;
 // menorMayor([4, 6, 1, 7, 15]) ==> [1, 15]
 
+const sortNum = numeros.sort(function(a,b){
+  return a - b;
+});
+var arr = [sortNum[0], sortNum[numeros.length - 1]];
+return arr;
+
 }
+
 
 function stringMasLarga(strings) {
   // Escribe una función llamada stringMasLarga, que tome un arreglo de strings llamado 'strings'
@@ -39,6 +47,18 @@ function stringMasLarga(strings) {
   // si hay dos strings del mismo tamanio devolve la que este primero en el arreglo (menor indice);
   // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
+
+var max = 0;
+  const numArr = strings.map(function(s){
+    return s.split('').length;
+  });
+
+  for(let i = 0; i < strings.length; i++){
+    if(max < numArr[i]){
+      max = numArr[i];
+    }
+  }
+  return strings[numArr.indexOf(max)];
 
 }
 
@@ -50,6 +70,15 @@ function aplastarArreglo(array) {
   // => [1, 2, 3, 4, 5, 6, 7, 8, 9]
   // pista: podes usar forEachs anidados;
 
+ var arr = [];
+  for(let i = 0; i < array.length; i++){
+    for(let x = 0; x < array[i].length; x++){
+      arr.push(array[i][x]);
+    }
+  }
+
+  return arr;
+
 }
 
 function pluck() { 
@@ -60,6 +89,12 @@ function pluck() {
 // var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
 // productos.pluck('name') // ['TV LCD', 'Computadora']
 // tip: es una buena oportunidad para usar map.
+
+ Array.prototype.pluck = function(arg){
+      return this.map(function(s){
+        return s[arg];
+      })
+  }
 
 
 }
@@ -85,12 +120,32 @@ function getFriends(personas, name) {
   // getFriends(personas, 'Martin') => ['toni', 'Leo', 'Manu']
   // tips: podes usar la funcion pluck que hiciste antes!
 
+var p = personas[0];
+    for(let i = 0; i < personas.length; i++){
+      if(personas[i].nombre === name){
+          p = personas[i];
+      }
+    }
+
+    var arr = [];
+
+    for(let i = 0; i < p.amigos.length; i++){
+      arr.push(p.amigos[i].nombre);
+    }
+
+    return arr ;
+
 }
 
 function getPromedioEdad(personas) {
   // Escribe una función que devuelva el promedio de edad de las personas en el arreglo 'personas'
   // tips: reduce!
   // iniciar el acc en 0! si no, por defecto cree que es un objeto!
+
+ return personas.reduce(function(acc, elem){
+      return Math.ceil((acc + elem.edad) / (personas.length - 1));
+  }, 0)
+
 
 }
 
@@ -107,6 +162,20 @@ function getEstadisticas(personas) {
   // tip: map!
   // podes usar un objeto literal como el de arriba e ir calculando ahi!
 
+var arrObj = [];
+  personas.forEach(function(s){
+    var obj = {}
+    obj.cantidadDeAmigos = s.amigos.length;
+    obj.cantidadDeHobbies = s.hobbies.length;
+    obj.cantidadDeLetrasDelNombre = s.nombre.length;
+    obj.anioDeNacimiento = 2020 - s.edad;
+
+    arrObj.push(obj);
+  })
+
+  return arrObj;
+
+
 };
 
 function crearClasePersona() {
@@ -119,7 +188,47 @@ function crearClasePersona() {
   // tip: Recuerden inicializar bien las propiedades que sean arreglos u objetos
   // usando el operador "||".
   // Esta funcion debe retonar la clase Persona.
-  
+  class Persona{
+    constructor(nombre, edad, hobbies, amigos){
+      this.nombre = nombre;
+      this.edad = edad;
+      this.hobbies = hobbies || [];
+      this.amigos = amigos || [];
+    }
+  }
+
+  Persona.prototype.addHobby = function(hobbie){
+    this.hobbies.push(hobbie);
+  }
+
+  Persona.prototype.addFriend = function(name){
+  var objFriend = {
+    nombre: name
+  }
+  this.amigos.push(objFriend);
+  }
+
+  Persona.prototype.getFriends = function(){
+    return this.amigos;
+  }
+
+  Persona.prototype.getEstadisticas = function(){
+    var obj = {}
+    obj.cantidadDeAmigos = this.amigos.length;
+    obj.cantidadDeHobbies = this.hobbies.length;
+    obj.cantidadDeLetrasDelNombre = this.nombre.length;
+    obj.anioDeNacimiento = 2020 - this.edad;
+    return obj;
+  }
+
+  Persona.prototype.getPromedioEdad = function(personas){
+    return personas.reduce(function(acc, elem){
+      return Math.ceil((acc + elem.edad) / (personas.length - 1));
+  }, 0)
+  }
+
+  return Persona;
+
 }
 
 
